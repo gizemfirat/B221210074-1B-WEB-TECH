ef6d55be67af40da13460e3abe8cdaa5e3af224c
gizem.firat <zgizem.zgf@gmail.com>
2023-04-30 16:23:17 +0300

    Stiller oluşturulup hakkımda kısmı yazıldı

--- server/index.js
+++ server/index.js
@@ -8,14 +8,49 @@ const port = 3000;
 app.use("/css", express.static(path.join(__dirname, "../css")));
 app.use("/js", express.static(path.join(__dirname, "../js")));
 app.use("/img", express.static(path.join(__dirname, "../img")));
-app.use("/node_modules", express.static(path.join(__dirname, "../node_modules")));
+app.use(
+  "/node_modules",
+  express.static(path.join(__dirname, "../node_modules"))
+);
 
-const indexHtml = fs.readFileSync(`html/index.html`, "utf-8");
+const data = {
+  name: "Gizem",
+  surname: "Fırat",
+};
 
 app.get("/", (req, res) => {
+  const indexHtml = fs.readFileSync(`html/index.html`, "utf-8");
+
   res.send(indexHtml);
 });
 
+app.get("/", (req, res) => {
+  const myCityHtml = fs.readFileSync(`html/myCity.html`, "utf-8");
+
+  res.send(myCityHtml);
+});
+
+app.get("/", (req, res) => {
+  const myInterestsHtml = fs.readFileSync(`html/myInsterests.html`, "utf-8");
+
+  res.send(myInterestsHtml);
+});
+
+app.get("/", (req, res) => {
+  const ourLegacyHtml = fs.readFileSync(`html/ourLegacy.html`, "utf-8");
+
+  res.send(ourLegacyHtml);
+});
+
+app.get("/mycv", (req, res) => {
+  const myCVHtml = fs.readFileSync(`html/myCV.html`, "utf-8");
+
+  var result = myCVHtml
+    .replace("{NAME}", data.name)
+    .replace("{SURNAME}", data.surname);
+
+  res.send(result);
+});
 
 app.listen(port, () => {
   console.log(`Application started at port ${port}`);
