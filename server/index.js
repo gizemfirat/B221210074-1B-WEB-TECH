d04f0f7089da5abb3114ea95f9a059c326acbaa9
gizem.firat <zgizem.zgf@gmail.com>
2023-05-11 17:49:51 +0300

    mirasımız sayfası yapıldı, mainpage yapısı kuruldu

--- server/index.js
+++ server/index.js
@@ -18,6 +18,12 @@ const data = {
   surname: "Fırat",
 };
 
+app.get("/masterpage", (req, res) => {
+  const masterPageHtml = fs.readFileSync(`html/masterPage.html`, "utf-8");
+
+  res.send(masterPageHtml);
+});
+
 app.get("/", (req, res) => {
   const indexHtml = fs.readFileSync(`html/index.html`, "utf-8");
 
@@ -30,32 +36,84 @@ app.get("/home", (req, res) => {
   res.send(indexHtml);
 });
 
+//app.get("/mycity", (req, res) => {
+ // const myCityHtml = fs.readFileSync(`html/myCity.html`, "utf-8");
+
+  //res.send(myCityHtml);
+//});
+
+//app.get("/myinsterests", (req, res) => {
+ // const myInterestsHtml = fs.readFileSync(`html/myInsterests.html`, "utf-8");
+
+ // res.send(myInterestsHtml);
+//});
+
+//app.get("/ourlegacy", (req, res) => {
+ // const ourLegacyHtml = fs.readFileSync(`html/ourLegacy.html`, "utf-8");
+
+ // res.send(ourLegacyHtml);
+//});
+
+app.get("/mycv", (req, res) => {
+  const myCVHtml = fs.readFileSync(`html/myCV.html`, "utf-8");
+
+  var layoutHtml = fs.readFileSync(`html/layout.html`, "utf-8");
+  layoutHtml = layoutHtml.replace("{RENDER_BODY}", myCVHtml);
+  layoutHtml = layoutHtml.replace("{PAGE_TITLE}", "Özgeçmiş");
+
+  var result = layoutHtml
+    .replace("{NAME}", data.name)
+    .replace("{SURNAME}", data.surname);
+
+  res.send(result);
+});
+
 app.get("/mycity", (req, res) => {
   const myCityHtml = fs.readFileSync(`html/myCity.html`, "utf-8");
 
-  res.send(myCityHtml);
+  var layoutHtml = fs.readFileSync(`html/layout.html`, "utf-8");
+  layoutHtml = layoutHtml.replace("{RENDER_BODY}", myCityHtml);
+  layoutHtml = layoutHtml.replace("{PAGE_TITLE}", "Şehrim");
+
+  var result = layoutHtml
+
+  res.send(result);
 });
 
-app.get("/myinsterests", (req, res) => {
-  const myInterestsHtml = fs.readFileSync(`html/myInsterests.html`, "utf-8");
+app.get("/myinterests", (req, res) => {
+  const myInterestsHtml = fs.readFileSync(`html/myInterests.html`, "utf-8");
 
-  res.send(myInterestsHtml);
+  var layoutHtml = fs.readFileSync(`html/layout.html`, "utf-8");
+  layoutHtml = layoutHtml.replace("{RENDER_BODY}", myInterestsHtml);
+  layoutHtml = layoutHtml.replace("{PAGE_TITLE}", "İlgi Alanlarım");
+
+  var result = layoutHtml
+
+  res.send(result);
 });
 
 app.get("/ourlegacy", (req, res) => {
   const ourLegacyHtml = fs.readFileSync(`html/ourLegacy.html`, "utf-8");
 
-  res.send(ourLegacyHtml);
+  var layoutHtml = fs.readFileSync(`html/layout.html`, "utf-8");
+  layoutHtml = layoutHtml.replace("{RENDER_BODY}", ourLegacyHtml);
+  layoutHtml = layoutHtml.replace("{PAGE_TITLE}", "Şehrim");
+
+  var result = layoutHtml
+
+  res.send(result);
 });
 
-app.get("/mycv", (req, res) => {
-  const myCVHtml = fs.readFileSync(`html/myCV.html`, "utf-8");
+app.get("/", (req, res) => {
+  const indexHtml = fs.readFileSync(`html/index.html`, "utf-8");
 
-  var result = myCVHtml
-    .replace("{NAME}", data.name)
-    .replace("{SURNAME}", data.surname);
+  var mainLayoutHtml = fs.readFileSync(`html/mainLayout.html`, "utf-8");
+  mainLayoutHtml = mainLayoutHtml.replace("{RENDER_BODY}", indexHtml);
+  mainLayoutHtml = mainLayoutHtml.replace("{PAGE_TITLE}", "Anasayfa");
 
-  res.send(myCVHtml);
+  var result = mainLayoutHtml
+  
+  res.send(result);
 });
 
 app.listen(port, () => {
